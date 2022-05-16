c5c0c0e5dd299612519314bbb88ef3bbe8a32f7c
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-05-16 15:58:21 +0200

    Forgot a semicolon

--- src/crypto.c
+++ src/crypto.c
@@ -131,6 +131,6 @@ bool initCrypto()
 	reseed();
 	spinCreateLock(rngLock, false);
 	return OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, NULL) == 1 &&
-		RAND_set_rand_method(&srm) == 1 //&&
+		RAND_set_rand_method(&srm) == 1; //&&
 		//RAND_DRBG_set_reseed_defaults(0, 0, 0, 0) == 1;
 }
